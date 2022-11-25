5cc22fe2ce59923510ea6c65afc9ff54a98ea035
Bluh7 <gabrielperazzo123456789@gmail.com>
2022-11-25 00:27:17 -0300

    new feature

--- main.py
+++ main.py
@@ -2,30 +2,45 @@ from os import system, name
 from password_gen import PasswordGenerator
 
 
-def clear(): 
-  system('cls' if name == 'nt' else 'clear')
+def clear():
+    system('cls' if name == 'nt' else 'clear')
 
 
 def main():
-  pg = PasswordGenerator()
-  clear()
-  print("Welcome to the PyPassword Generator!")
-  pg.get_nr_letters()
-  pg.get_nr_upper_letters()
-  pg.get_nr_symbols()
-  pg.get_nr_numbers()
-  pg.generate_password()
-  pg.print_password()
-  save_password = input("\nDo you want to save this password? (y/n)\n")
-  if save_password == 'y' or save_password == 'Y':
-      pg.get_password_name()
-      pg.save_password()
-  create_another = input("\nDo you want to generate another password? (y/n)\n")
-  if create_another == 'y' or create_another == 'Y':
-      main()
-  input("\nPress enter to exit\n")
-  clear()
+    while True:
+        pg = PasswordGenerator()
+        clear()
+        print("Welcome to the PyPassword Generator!")
+        pg.get_nr_letters()
+        pg.get_nr_upper_letters()
+        pg.get_nr_symbols()
+        pg.get_nr_numbers()
+        while True:
+            pg.generate_password()
+            pg.print_password()
+            if len(pg.password) > 0:
+                save_password = input(
+                    "\nDo you want to save this password? (y/n)\n").lower()
+                if save_password == 'y':
+                    pg.get_password_name()
+                    pg.save_password()
+            create_another = input(
+                "\nDo you want to generate another password? (y/n)\n").lower()
+            if create_another == 'y':
+                same_settings = input(
+                    "\nDo you want to use the same settings? (y/n)\n").lower()
+                if same_settings == 'y':
+                    clear()
+                    pg.password_list = []
+                    pg.password = ""
+                    continue
+                else:
+                    main()
+            else:
+                input("\nPress enter to exit\n")
+                clear()
+                exit()
 
 
 if __name__ == "__main__":
-  main()
+    main()
--- password_gen.py
+++ password_gen.py
@@ -3,64 +3,61 @@ import random
 
 class PasswordGenerator:
     def __init__(self) -> None:
-        self.LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         self.UPPER_LETTERS = [letter.upper() for letter in self.LETTERS]
         self.NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        self.SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '>', '<', '^', '~', '@', '-', '_', 'ç', 'Ç','`', '/', '|', 'ª', 'º', '¿',]
+        self.SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '>', '<',
+                        '^', '~', '@', '-', '_', 'ç', 'Ç', '`', '/', '|', 'ª', 'º', '¿',]
         self.password_list = []
         self.password = ""
-        self.password_name = ""
+        self.password_name = None
         self.nr_letters = None
         self.nr_upper_letters = None
         self.nr_symbols = None
         self.nr_numbers = None
 
-
     def get_nr_letters(self) -> None:
         while True:
             try:
-                if self.nr_letters == None:
-                    self.nr_letters = int(input("\nHow many letters would you like in your password?\n")) 
+                self.nr_letters = int(
+                    input("\nHow many letters would you like?\n"))
                 break
             except ValueError:
                 print("\nPlease, enter a number")
-
 
     def get_nr_upper_letters(self) -> None:
         while True:
             try:
-                if self.nr_upper_letters == None:
-                    self.nr_upper_letters = int(input("\nHow many upper letters would you like?\n"))
+                self.nr_upper_letters = int(
+                    input("\nHow many upper letters would you like?\n"))
                 break
             except ValueError:
                 print("\nPlease, enter a number")
-
 
     def get_nr_symbols(self) -> None:
         while True:
             try:
-                if self.nr_symbols == None:
-                    self.nr_symbols = int(input("\nHow many symbols would you like?\n"))
+                self.nr_symbols = int(
+                    input("\nHow many symbols would you like?\n"))
                 break
             except ValueError:
                 print("\nPlease, enter a number")
-
 
     def get_nr_numbers(self) -> None:
         while True:
             try:
-                if self.nr_numbers == None:
-                    self.nr_numbers = int(input("\nHow many numbers would you like?\n"))
+                self.nr_numbers = int(
+                    input("\nHow many numbers would you like?\n"))
                 break
             except ValueError:
                 print("\nPlease, enter a number")
 
-
     def generate_password(self) -> None:
         for char in range(1, self.nr_letters + 1):
-            self.password_list.append(random.choice(self.LETTERS))
+            self.password_list += random.choice(self.LETTERS)
         for char in range(1, self.nr_upper_letters + 1):
-            self.password_list += (random.choice(self.UPPER_LETTERS))
+            self.password_list += random.choice(self.UPPER_LETTERS)
         for char in range(1, self.nr_symbols + 1):
             self.password_list += random.choice(self.SYMBOLS)
         for char in range(1, self.nr_numbers + 1):
@@ -70,16 +67,12 @@ class PasswordGenerator:
         for char in self.password_list:
             self.password += char
 
-
     def print_password(self) -> None:
         print(f"\nYour password is: \n{self.password}")
-
 
     def get_password_name(self) -> None:
         self.password_name = input("\nName this password\n")
 
-
     def save_password(self) -> None:
         with open("my passwords.txt", "a", encoding="utf-8") as file:
             file.write(f"{self.password_name}\n{self.password}\n\n")
-
